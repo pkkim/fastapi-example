f0f7fb0dd0800315821e060df0725b8a6e9d8239
Paul Kim <paul@silogy.io>
2024-01-03 10:37:08 -0500

    Test connecting to database

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import psycopg2
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,4 +13,9 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    conn = psycopg2.connect(host="database-1.cytow4gkazsz.us-west-1.rds.amazonaws.com", port=5432, database="postgres", user="postgres", password="mypassword")
+    cur = conn.cursor()
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    return {"item_id": item_id, "q": q, "db_version": db_version}
